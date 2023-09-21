5c112419ad5d532f2417e386255392cf005b9cc1
ashkan0201 <ashkan02011@gmial.com>
2023-09-21 12:08:25 +0330

    added create a table

--- How_old_am_i.py
+++ How_old_am_i.py
@@ -3,3 +3,9 @@
 # Required libraries
 from datetime import datetime
 from tabulate import tabulate
+
+today = datetime.today()
+
+# Create a table
+table = [[today.strftime(" Dedicated to the most important person in my life (: \nFor example =>>> %Y/%m/%d  \nUse the 'Ctrl + c' or write: 'exit' to finish\n")]]
+print(tabulate(table, tablefmt="grid"))
